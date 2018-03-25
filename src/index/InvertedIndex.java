751c3836cde8cfd5d02ff7b19ac354e0183b848d
GatoY <derakhy@gmail.com>
2018-03-26 01:47:51 +1100

    simplify

--- src/index/InvertedIndex.java
+++ src/index/InvertedIndex.java
@@ -1,66 +1,31 @@
 package index;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Hashtable;
 import java.util.Iterator;
 
 public class InvertedIndex { 
-	
-	private String FilePath;
 	private Hashtable<String, InvertedList> wordList=new Hashtable<String, InvertedList>();
-	private String toFilePath;
-	
-
-	public InvertedIndex(String fromFilePath, String toFilePath) {
-		this.FilePath=fromFilePath;
-		this.toFilePath=toFilePath;
+	public InvertedIndex() {		
 	}
-	public void readAndDeal() throws IOException {
-		//System.out.println("Start readAndDeal");
-        FileReader fr=new FileReader(this.FilePath);
-        BufferedReader br=new BufferedReader(fr);
-        String line="";
-        String[] words;
-        Integer FileNo = 0;
-        while ((line=br.readLine())!=null) {
-        		//System.out.println(line.length());
-            words=line.split(" ");
-            //System.out.println("words split done");
-            //System.out.println(line);
-            //System.out.println(words.length);
-            //System.out.println(words[0]);
-            for(int i =0;i<words.length;i++) {
-            		//System.out.println(words[i]);
-            		if(words[i].length()>=6) {
-            			//System.out.println(words[i].substring(0, 6));
-            			if(words[i].substring(0, 6).equals("FileNo")) {
-            				//System.out.println("continue");
-            				FileNo++;
-            				continue;
-            			}
-            		}
-            		if(!this.wordList.containsKey(words[i])) {
-            			
-            			InvertedList word = new InvertedList(FileNo);
-            			//System.out.println("1");
-            			this.wordList.put(words[i], word);
-            		}
-          		else {
-            			this.wordList.get(words[i]).add(FileNo);
-            		}
-       		}    
-        }
-        System.out.println("read and deal done");
+	public void readAndDeal(String[] words, Integer FileNo) throws IOException {
+	    for(int i =0;i<words.length;i++) {
+			if(!this.wordList.containsKey(words[i])) {
+				InvertedList word = new InvertedList(FileNo);
+				this.wordList.put(words[i], word);
+			}
+			else {
+				this.wordList.get(words[i]).add(FileNo);
+			}
+		}    
 	}
 	
 	public void writeResult() throws IOException {
 		System.out.println("start write");
 		Iterator<String> keyList = this.wordList.keySet().iterator();
-		FileWriter lexicon = new FileWriter(toFilePath+"lexicon.txt",false);
-		FileWriter invlists = new FileWriter(toFilePath+"invlists.txt",false);
+		FileWriter lexicon = new FileWriter("lexicon.txt",false);
+		FileWriter invlists = new FileWriter("invlists.txt",false);
 		//FileWriter map = new FileWriter("map.txt",false);
 		//countOfInvertedList shows which line of invertedList of the word lies in invlists.txt
 		Integer countOfInvertedList=1;
@@ -78,4 +43,31 @@ public class InvertedIndex {
 		invlists.close();
 		System.out.println("write done");
 	}
+	
+	public void writeResultWithStoppers(Hashtable<String, Integer> stoppers) throws IOException {
+		System.out.println("start write");
+		Iterator<String> keyList = this.wordList.keySet().iterator();
+		FileWriter lexicon = new FileWriter("lexicon.txt",false);
+		FileWriter invlists = new FileWriter("invlists.txt",false);
+		//FileWriter map = new FileWriter("map.txt",false);
+		//countOfInvertedList shows which line of invertedList of the word lies in invlists.txt
+		Integer countOfInvertedList=1;
+		System.out.println(wordList.size());
+		while(keyList.hasNext()) {
+			String word = keyList.next();
+			String result = this.wordList.get(word).result();
+			if(stoppers.containsKey(word)) {
+				continue;
+			}
+			System.out.println(word);
+			lexicon.write(word+countOfInvertedList.toString()+"\t\n");
+			invlists.write(result+"\t\n");
+			countOfInvertedList++;
+		}
+		
+		lexicon.close();
+		invlists.close();
+		System.out.println("write done");
+	}
+	
 }
--- src/index/index.java
+++ src/index/index.java
@@ -1,73 +1,46 @@
 package index;
-import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.regex.*;
 import java.io.*;
 public class index {
 	//main function
 	public static void main( String args[]) {
-		
 		//start
 		long startTime=System.currentTimeMillis();
-		
 		String pattern ="(<TEXT>([\\s\\S]*?)</TEXT>)|(<HEADLINE>([\\s\\S]*?)</HEADLINE>)|(<DOCNO>([\\s\\S]*?)</DOCNO>)";
-		
 		//read file.
 		String textFile = readFileByChars("/Users/Jason/desktop/javaMelb/Index Tool/src/index/latimes");
 		System.out.println("read successful");
-		
-		//get result of Re and deal with it.
-		resultOfRe[] outcome =  matchFile(pattern, textFile);
-		
+		InvertedIndex invertedindex = new InvertedIndex();
 		try {
-			removeAndCF(outcome);
-		} catch (IOException e) {
+			matchFile(pattern, textFile, invertedindex);
+		} catch (IOException e1) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			e1.printStackTrace();
 		}
-		outcome=null;
-		
-		
 		//String textFile = readFileByChars("/Users/Jason/desktop/javaMelb/Index Tool/src/index/latimes");
-		
-		if(true) {
+		int i = 1;
+		if(i==2) {
 			System.out.println("Start stopping");
-			String filename = "/Users/Jason/desktop/javaMelb/Index Tool/src/index/stoplist";
+			String filename = "/Users/Jason/desktop/javaMelb/Index Tool/src/index/stoplist";			
+			Hashtable<String, Integer> stoppers = new Hashtable<String, Integer>();
 			try {
-				Hashtable<String, Integer> stoppers = readAndHash(filename);			
-				matchFileAndPrint(stoppers,"/Users/Jason/desktop/javaMelb/Index Tool/src/index/text.txt");
-				matchFileAndPrint(stoppers, "/Users/Jason/desktop/javaMelb/Index Tool/src/index/headline.txt");
-				}
-			catch (IOException e) {
+				stoppers = readAndHash(filename);
+			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			}
-			
+			}			
+			//stoppers.containsKey(words[i]);
+			try {
+				invertedindex.writeResultWithStoppers(stoppers);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}		
 		}
-		
-		String fromFilePathOfText="/Users/Jason/desktop/javaMelb/Index Tool/text.txt";
-		String toFilePathOfText="/Users/Jason/desktop/javaMelb/Index Tool/Text";
-		String fromFilePathOfHeadLine="/Users/Jason/desktop/javaMelb/Index Tool/headline.txt";
-		String toFilePathOfHeadLine="/Users/Jason/desktop/javaMelb/Index Tool/Headline";
-		
-		
-		InvertedIndex invertedindexText = new InvertedIndex(fromFilePathOfText, toFilePathOfText);
-		InvertedIndex invertedindexHeadline =new InvertedIndex(fromFilePathOfHeadLine, toFilePathOfHeadLine);
+		else {
 		try {
-			System.out.println("Start headline read and deal");
-			
-			invertedindexText.readAndDeal();
-			System.out.println("text read and deal done");
-			invertedindexText.writeResult();
-			System.out.println("InvertedText done");
-			
-			invertedindexHeadline.readAndDeal();
-			invertedindexText.writeResult();
-			System.out.println("InvertedHeadline done");
-			
-			
-			
-			
+			invertedindex.writeResult();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -75,96 +48,40 @@ public class index {
 		System.out.println("match and deal successfully");
 		long endTime=System.currentTimeMillis();
 		System.out.println("程序运行时间： "+(endTime-startTime)+"ms");
-		
 	}
-	
-/*	public static void matchFileAndPrint2(Hashtable<String, Integer> stoppers, String filePath) throws IOException {
-		
-		FileReader fr=new FileReader(filePath);
-        BufferedReader br=new BufferedReader(fr);
-        String line="";
-        System.out.println("match and print start");
-        while ((line=br.readLine())!=null) {
-        		System.out.println(line.length());
-        		break;
-        }
-        br.close();
-        fr.close();
-		System.out.println("hash dealing done");
-	}
-*/	
-public static void matchFileAndPrint(Hashtable<String, Integer> stoppers, String filePath) throws IOException {
-        File file = new File(filePath);
-        if(!file.exists() || !file.isFile()) {
-    			System.out.println("No file");
-    			return;
-    		}
-    		try {
-    			char[] temp = new char[1024];
-    			FileInputStream fileInputStream = new FileInputStream(file);
-    			InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, "GBK");
-    		
-    			while(inputStreamReader.read(temp) != -1) {
-    				String[] words = new String(temp).split(" ");
-    				//System.out.println(new String(temp));
-    				//System.out.println(stoppers.get(words[0]));
-    				for(int i=0; i<words.length;i++) {
-    					//System.out.println(i);
-    					//System.out.println(words[i]);
-    					if(stoppers.containsKey(words[i])) {
-    						//System.out.println(2);
-    						System.out.println(words[i]);
-    					}
-    				}
-    				temp = new char[1024];
-    		}
-    		fileInputStream.close();
-    		inputStreamReader.close();
-    	}catch(FileNotFoundException e) {
-    		e.printStackTrace();
-    	}catch(IOException e) {
-    		e.printStackTrace();
-    	}   
-		System.out.println("hash dealing done");
 	}
 	
 	
 	
-	//match file;
-	public static resultOfRe[] matchFile(String pattern, String text) {
-		ArrayList<String> text_terms = new ArrayList<String>();
-		ArrayList<Integer> text_start = new ArrayList<Integer>();
-		ArrayList<String> headline_terms = new ArrayList<String>();
-		ArrayList<Integer> headline_start = new ArrayList<Integer>();
-		ArrayList<String> doc_terms = new ArrayList<String>();
-		ArrayList<Integer> doc_start = new ArrayList<Integer>();
+	public static void matchFile(String pattern, String text, InvertedIndex invertedindex) throws IOException {
+		String terms = "";
 		Pattern r=Pattern.compile(pattern);
 		Matcher m=r.matcher(text);
+		Integer count = 0;
+		FileWriter map = new FileWriter("map.txt",false);
 		while(m.find()) {
+			if(m.group(6)!=null) {
+				map.write(count+" "+m.group(6)+"\t\n");
+				map.flush();
+				count++;
+				terms=terms+"FileNo"+count.toString()+" ";
+		}
 			
 			if(m.group(2) != null) {
-				text_terms.add(m.group(2));
-				text_start.add(m.start(2));
+				String[] words =m.group(2).replaceAll("\\t|\r|\n|<.*?>|\\pP", "").toLowerCase().split(" ");
+				invertedindex.readAndDeal(words,count);
+				
 				//System.out.println(m.group(2));
 			}
 			if(m.group(4)!=null) {
-				headline_terms.add(m.group(4));
-				headline_start.add(m.start(4));
+				String[] words =m.group(4).replaceAll("\\t|\r|\n|<.*?>|\\pP", "").toLowerCase().split(" ");
+				invertedindex.readAndDeal(words,count);
 				//System.out.println(m.group(4));
 			}
-			if(m.group(6)!=null) {
-				doc_terms.add(m.group(6));
-				doc_start.add(m.start(6));
-		}
 			}
-		resultOfRe[] outcome = new resultOfRe[3];
-		outcome[0] = new resultOfRe(text_terms, text_start);
-		outcome[1] = new resultOfRe(headline_terms, headline_start);
-		outcome[2] = new resultOfRe(doc_terms, doc_start);
-		return outcome;
-		
+		map.close();
 	}
-	
+
 	public static String readFileByChars(String filePath) {
 		File file = new File(filePath);
 		if(!file.exists() || !file.isFile()) {
@@ -192,95 +109,6 @@ public static void matchFileAndPrint(Hashtable<String, Integer> stoppers, String
 		return content.toString();
 	}
 	
-	public static void removeAndCF(resultOfRe[] outcome) throws IOException{
-		ArrayList<String> terms_text = outcome[0].getTerms();
-		ArrayList<Integer> start_text = outcome[0].getStart();
-		ArrayList<String> terms_headline = outcome[1].getTerms();
-		ArrayList<Integer> start_headline = outcome[1].getStart();
-		ArrayList<String> terms_doc = outcome[2].getTerms();
-		ArrayList<Integer> start_doc = outcome[2].getStart();
-		int size_text = terms_text.size();
-		System.out.println("size of text"+Integer.toString(size_text));
-		int size_headline = terms_headline.size();
-		System.out.println("size of headline"+Integer.toString(size_headline));
-		int size_doc=terms_doc.size();
-		System.out.println("size of map" + Integer.toString(size_doc));
-		int i = 0, j = 0, w = 0;
-		FileWriter file_text = new FileWriter("text.txt",false);
-		FileWriter file_headline = new FileWriter("headline.txt",false);
-		FileWriter file_map = new FileWriter("map.txt",false);
-		System.out.println("Start deal with files");
-		while(i<size_doc-1) {
-			if(j<size_text) {
-				//System.out.println("text write");
-			file_text.write("FileNo"+Integer.toString(i)+" "+terms_text.get(j).replaceAll("\\t|\r|\n|<.*?>|\\pP", "").toLowerCase()+"\t\n");
-			j++;
-			//System.out.println(j);
-			//System.out.println(size_text);
-			
-			//	System.out.println(1);
-			//	System.out.println(Integer.toString(start_text.get(j)));
-			//System.out.println(Integer.toString(start_doc.get(i+1)));
-				
-				while(j<size_text){
-					//System.out.println("deal with text");					
-                    	if(start_text.get(j)>start_doc.get(i+1)) {          		
-                    		break;
-                    	}
-                    	file_text.write(terms_text.get(j).replaceAll("\\t|\r|\n|<.*?>|\\pP", "").toLowerCase()+"\t\n");
-                    	j++;
-            }
-			}
-
-			if(w<size_headline) {
-				file_headline.write("FileNo"+Integer.toString(i)+" "+terms_headline.get(w).replaceAll("\\t|\r|\n|<.*?>|\\pP", "").toLowerCase()+"\t\n");
-				w++;
-				while(w<size_headline){ 
-					if(start_headline.get(w)>start_doc.get(i+1)){
-						break;
-					}	
-					//System.out.println("111111");
-					file_headline.write(terms_headline.get(w).replaceAll("\\t|\r|\n|<.*?>|\\pP", "").toLowerCase()+"\t\n");
-					w++;
-				}
-			}
-			
-			file_map.write(Integer.toString(i)+terms_doc.get(i)+"\t\n");
-			file_text.flush();
-			file_headline.flush();
-			file_map.flush();
-			i++;
-		}
-		
-		if(j<size_text) {
-			//System.out.println("text write");
-			file_text.write(Integer.toString(i)+terms_text.get(j).replaceAll("\\t|\r|\n|<.*?>|\\pP", "").toLowerCase()+"\t\n");
-			j++;
-		}
-		
-		if(w<size_headline) {
-			//System.out.println("headline write");
-			file_headline.write(Integer.toString(i)+terms_headline.get(w).replaceAll("\\t|\r|\n|<.*?>|\\pP", "").toLowerCase()+"\t\n");
-			w++;
-		}
-		
-		
-		while(j<size_text) {
-			file_text.write(terms_text.get(j).replaceAll("\\t|\r|\n|<.*?>|\\pP", "").toLowerCase()+"\t\n");
-			j++;
-		}
-		
-		while(w<size_headline) {
-			file_headline.write(terms_headline.get(w).replaceAll("\\t|\r|\n|<.*?>|\\pP", "").toLowerCase()+"\t\n");
-			w++;
-		}
-		
-		file_text.close();
-		file_headline.close();
-		file_map.close();
-		System.out.println("finish indexing");
-	}
-	
 	public static Hashtable<String, Integer> readAndHash(String filename) throws IOException {
 		Hashtable<String, Integer> stoppers = new Hashtable<String, Integer>();
 		FileReader fr=new FileReader(filename);
@@ -295,6 +123,4 @@ public static void matchFileAndPrint(Hashtable<String, Integer> stoppers, String
         System.out.println("get stoppers");
         return stoppers;
     }
-
-	
 }  	
